4ae7086a9a5f0f74211a140f2be573ebdfa62e84
francoSerafini <frankitooserafini493@gmail.com>
2022-12-05 21:15:35 -0300

    refactor apiFindFogById

--- api/src/routes/Race/controller.js
+++ api/src/routes/Race/controller.js
@@ -1,27 +1,12 @@
 require('dotenv').config();
 const { API_KEY } = process.env;
-const axios = require('axios');
+//const axios = require('axios');
 const { Race, Temperament, Op } = require('../../db');
 const { createTemperaments } = require('../Temperament/controller');
-const { apiAllDogs } = require('../Race/auxFunctions/apiAllDogs')
+const { apiAllDogs } = require('../Race/auxFunctions/apiAllDogs');
+const { apiFindDogById } = require('../Race/auxFunctions/apiFindDogById');
 
 let cont = 0;   
-
-const apiFindDogById = async function(id) {
-    let dog = await axios(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
-    dog = dog.data.find(dog => dog.id == id);
-        if (dog) {
-            dog = {
-                name: dog.name,
-                height: dog.height.metric,
-                weight: dog.weight.metric,
-                life_span: dog.life_span,
-                temperaments: dog.temperament,
-                image: dog.image ? dog.image.url : 'Not Found'
-            };
-        };
-        return dog;
-};
 
 const concatTemperamentsSingle = function(dog) {
     let temp = '';
