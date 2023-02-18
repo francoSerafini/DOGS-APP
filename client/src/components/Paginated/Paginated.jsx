f8d686932cdede9435b5ecb9dc2e6b0c4f4fae3b
francoSerafini <frankitooserafini493@gmail.com>
2023-02-18 18:15:48 -0300

    'refactor'

--- client/src/components/Paginated/Paginated.jsx
+++ client/src/components/Paginated/Paginated.jsx
@@ -10,12 +10,12 @@ const Paginated = (props) => {
     let [currentPage, setCurrentPage] = useState(0);
 
     function awaitDogs() {
-        setItems([...props.dogsToShow].splice(0, 8));
+        setItems([...props.dogsToShow].splice(0, 6));
         setCurrentPage(0);
     };
    
-    const totalPages = Math.floor(props.dogsToShow.length / 8) !== 0 ?
-        Math.floor(props.dogsToShow.length / 8) + 1 : 1;
+    const totalPages = Math.floor(props.dogsToShow.length / 6) !== 0 ?
+        Math.floor(props.dogsToShow.length / 6) + 1 : 1;
     
     const current = (currentPage + 1) > totalPages ?
         1 : currentPage + 1;
@@ -23,17 +23,17 @@ const Paginated = (props) => {
     function nextHandler() {
         const total = props.dogsToShow.length;
         const nextPage = currentPage + 1;
-        const firstIndex = nextPage * 8;
+        const firstIndex = nextPage * 6;
         if(firstIndex >= total) return;
-        setItems([...props.dogsToShow].splice(firstIndex, 8));
+        setItems([...props.dogsToShow].splice(firstIndex, 6));
         setCurrentPage(nextPage);
     }; 
   
     function prevHandler() {
         const prevPage = currentPage - 1;
         if (prevPage < 0) return;
-        const firstIndex = prevPage * 8;
-        setItems([...props.dogsToShow].splice(firstIndex, 8));
+        const firstIndex = prevPage * 6;
+        setItems([...props.dogsToShow].splice(firstIndex, 6));
         setCurrentPage(prevPage);
     };
 
